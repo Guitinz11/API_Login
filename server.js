1321f25fca25fe785a3ab3eeba33dd7c53649b85
biro99 <biel49412@gmail.com>
2026-06-08 08:25:15 -0300

    .

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
+    console.log(`Servidor rodando na porta 3000`);
 });
